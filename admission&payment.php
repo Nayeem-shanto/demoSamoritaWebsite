acf410317fa0695c58e7907b83cf4aff66d1b6ae
Nayeem Hasan Shanto <snayeemhasan@gmail.com>
2025-08-07 12:49:45 +0600

    admission and page updated

--- admission&payment.php
+++ admission&payment.php
@@ -1,47 +1,50 @@
-<?php include 'headerhospital.php';?>
+<?php include 'headerhospital.php'; ?>
 
 <!--==============================
     Breadcumb
 ============================== -->
-    <div class="breadcumb-wrapper ">
-        <div class="parallax" data-parallax-image="assets/img/breadcurmb/breadcurmb-1-1.jpg"></div>
-        <div class="container z-index-common">
-            <div class="breadcumb-content">
-                
-                <div class="breadcumb-menu-wrap">
-                    <i class="far fa-home-lg"></i>
-                    <ul class="breadcumb-menu">
-                        <li><a href="index4.php">Home</a></li>
-                        <li><a href="admission&payment.php">Admission & Payment</a></li>
-                    
-                    </ul>
-                </div>
+<div class="breadcumb-wrapper ">
+    <div class="parallax" data-parallax-image="assets/img/breadcurmb/breadcurmb-1-1.jpg"></div>
+    <div class="container z-index-common">
+        <div class="breadcumb-content">
+
+            <div class="breadcumb-menu-wrap">
+                <i class="far fa-home-lg"></i>
+                <ul class="breadcumb-menu">
+                    <li><a href="index4.php">Home</a></li>
+                    <li><a href="admission&payment.php">Admission & Payment</a></li>
+
+                </ul>
             </div>
         </div>
     </div>
+</div>
 
 
-    <main style="margin:auto;  padding-left: 150px; padding-right: 150px; background-image: url(./assets/img/bg/bg-shape-8.jpg); text-align:justify">
-        <section>
+<main style="margin:auto;  padding-left: 150px; padding-right: 150px; text-align:justify">
+    <section>
         <!-- Admission section -->
-        <div>
-            <h3 style="border: 1px solid; text-align:center">Admission</h3>
+        <div class="admission&payment sm-col-1" style="display: flex; gap:100px; ">
+            <div style=" padding:20px; border-radius:20px; margin:10px; box-shadow: 5px 5px 15px rgba(47, 46, 46, 0.3); background-color:azure;">
+            <h3 style="text-align:center">Admission</h3>
             <p style="color: black;">At MHSHMC, patients can be admitted as an in-patient either from out-patient department (OPD) or from Emergency.
                 After consultation with the patient in OPD, depending on patient’s condition and need, the Consultant may advice the patient for admission. In emergency cases, after primary evaluation, the patient might require admission in ward or critical care departments like HDU or ICU. Primary decision maker EMO In both cases, the hospital will provide you an Admission Form. Unusual patient like accidental / homicidal/ Suicidal attempt admission must informed police station
             </p>
         </div>
 
-        <div>
-            <h3 style="border: 1px solid; text-align:center">Before Admission</h3>
-            <p>
-                Aside from giving you the Admission Form, your consultant will advice you on the treatment plan and expected length of hospital stay before admission. You are requested to arrange necessary financial requirement prior to your admission. Based on treatment plan and the expected length of hospital stay, the billing estimate can be collected from our Billing Department. You can visit our Admission Desk for queries or information on admission process or you can call our Admission Desk at +88 02-8878080.                      .
+        <div style="padding:20px; border-radius:20px; margin:10px; box-shadow: 5px 5px 15px rgba(47, 46, 46, 0.3); background-color:azure">
+            <h3 style="text-align:center">Before Admission</h3>
+            <p style="color: black;">
+                Aside from giving you the Admission Form, your consultant will advice you on the treatment plan and expected length of hospital stay before admission. You are requested to arrange necessary financial requirement prior to your admission. Based on treatment plan and the expected length of hospital stay, the billing estimate can be collected from our Billing Department. You can visit our Admission Desk for queries or information on admission process or you can call our Admission Desk at +88 02-8878080. .
             </p>
         </div>
-        <div>
+        </div>
+        <div style="max-width: 1440px; margin: 0 auto">
+            <div>
             <h3 style="border: 1px solid; text-align:center">Documents to Bring for Admission</h3>
             <p>
-             For admission, you need to present consultants advise (prescription) and fill up the Admission Form given to you from the admission desk. For foreign patients, please bring your passport. Corporate patients are required to produce the organization’s employee ID card at the Admission Desk for availing premium service. For corporate clients having credit facility agreement with MHSHMC for in-patient service, GOP (Guarantee of Payment) or LOA (Letter of Authorization) will be required from their organization to avail the same.
-   
+                For admission, you need to present consultants advise (prescription) and fill up the Admission Form given to you from the admission desk. For foreign patients, please bring your passport. Corporate patients are required to produce the organization’s employee ID card at the Admission Desk for availing premium service. For corporate clients having credit facility agreement with MHSHMC for in-patient service, GOP (Guarantee of Payment) or LOA (Letter of Authorization) will be required from their organization to avail the same.
+
             </p>
         </div>
         <div>
@@ -65,7 +68,7 @@
         <div>
             <h3>Checking Out Time</h3>
             <p>
-               Checking out time will be at 12:00 noon. After 12 noon, next day’s rent will be calculated automatically by software. 
+                Checking out time will be at 12:00 noon. After 12 noon, next day’s rent will be calculated automatically by software.
             </p>
         </div>
 
@@ -90,18 +93,19 @@
                 Before operation, OT charge will have to be paid to ensure surgery on next day. Please note, this is essential for prior planning and scheduling of operations in order.
             </p>
         </div>
-        </section>
+    </section>
 
-        <!-- Billing and Payment Section -->
-         <section>
-            <div>
-                <h3>Billing & Payment</h3>
-                <p>
-                   Depending upon the room requested and nature of the treatment planned for the patient, an initial deposit will be collected at the time of admission for which a receipt will be issued to you and this will be adjusted in your final bill. In the case of patients who are being admitted for any surgery/package service, the entire amount is payable at the time of admission. In case of visiting consultants patient initial deposit must consult  with respective consultant. We accept cash  as well as credit cards. We do not, however, accept cheques. While the patient is admitted, at any point the updated provisional bill can be collected from our IPD Billing Section at Level  4. Once your bill exceeds your deposit amount, you will be approached by our Billing Executives for further deposit. We request you to pay within 24 hours to avoid any future inconvenience. Before final billing decision must pay pharmacydue & take clearance from pharmacy. Corporate Patients having in-patient credit agreement with the hospital, who cannot produce LOA (Letter of Authorization), will have to pay the initial deposit, which will be refunded subsequently upon producing the LOA. For payments, you may contact our billing personnel at level-12 between 9:00 am to 5:00 pm. Payment should be made only to Billing Department personnel and not to any other individual. For any clarification regarding billing, please contact Billing Executive at +88 02-8878080 authority.
- 
-                </p>
-            </div>
-         </section>
-    </main>
+    <!-- Billing and Payment Section -->
+    <section>
+        <div>
+            <h3>Billing & Payment</h3>
+            <p>
+                Depending upon the room requested and nature of the treatment planned for the patient, an initial deposit will be collected at the time of admission for which a receipt will be issued to you and this will be adjusted in your final bill. In the case of patients who are being admitted for any surgery/package service, the entire amount is payable at the time of admission. In case of visiting consultants patient initial deposit must consult with respective consultant. We accept cash as well as credit cards. We do not, however, accept cheques. While the patient is admitted, at any point the updated provisional bill can be collected from our IPD Billing Section at Level 4. Once your bill exceeds your deposit amount, you will be approached by our Billing Executives for further deposit. We request you to pay within 24 hours to avoid any future inconvenience. Before final billing decision must pay pharmacydue & take clearance from pharmacy. Corporate Patients having in-patient credit agreement with the hospital, who cannot produce LOA (Letter of Authorization), will have to pay the initial deposit, which will be refunded subsequently upon producing the LOA. For payments, you may contact our billing personnel at level-12 between 9:00 am to 5:00 pm. Payment should be made only to Billing Department personnel and not to any other individual. For any clarification regarding billing, please contact Billing Executive at +88 02-8878080 authority.
 
-    <?php include 'footerhospital.php';?>
+            </p>
+        </div>
+        </div>
+    </section>
+</main>
+
+<?php include 'footerhospital.php'; ?>
