6e1921106e5d7055455cde5f5a15633a71852c16
Nayeem Hasan Shanto <snayeemhasan@gmail.com>
2025-08-24 16:26:47 +0600

    principal sir profile updated

--- dr.abduljalilansari.php
+++ dr.abduljalilansari.php
@@ -1,218 +1,200 @@
-<?php include 'headerhospital.php';?>
-    <!--==============================
+<?php include 'headerhospital.php'; ?>
+<!--==============================
     Breadcumb
 ============================== -->
-    <div class="breadcumb-wrapper">
-      <div
-        class="parallax"
-        data-parallax-image="assets/img/breadcurmb/breadcurmb-1-1.jpg"
-      ></div>
-      <div class="container z-index-common">
-        <div class="breadcumb-content">
-          <h1 class="breadcumb-title" style="color: whitesmoke;"></h1>
-          <div class="breadcumb-menu-wrap">
-            <i class="far fa-home-lg"></i>
-            <ul class="breadcumb-menu">
-              <li><a href="index-4.php">Home</a></li>
-              <li><a href="alldoctor.php">Doctor Details</a></li>
-              
-            </ul>
+<div class="breadcumb-wrapper">
+  <div
+    class="parallax"
+    data-parallax-image="assets/img/breadcurmb/breadcurmb-1-1.jpg"></div>
+  <div class="container z-index-common">
+    <div class="breadcumb-content">
+      <h1 class="breadcumb-title" style="color: whitesmoke;"></h1>
+      <div class="breadcumb-menu-wrap">
+        <i class="far fa-home-lg"></i>
+        <ul class="breadcumb-menu">
+          <li><a href="index-4.php">Home</a></li>
+          <li><a href="alldoctor.php">Doctor Details</a></li>
+
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
+<!--==============================
+    Team Details
+    ==============================-->
+<section class="vs-details-wrapper space-top space-md-bottom">
+  <div class="container">
+    <div class="row gx-40">
+      <div class="col-lg-5">
+        <div
+          class="member-header mb-40 overflow-hidden rounded-3 position-relative">
+          <div class="member-details-img">
+
+            <img
+              src="assets/img/team/abduljalailansari.png"
+              alt="Member Image"
+              class="w-100" />
           </div>
+
+          <div class="member-angle-links">
+            <div class="shape">
+              <img src="assets/img/bg/member-shape.png" alt="Shape" />
+            </div>
+
+          </div>
+
+          <a href="#" class="certifate-link"><span style="color: #07ccec; :hover {color: #FFFFFF;}">BMDC Registration Number: A-14668 </span><i
+              class="far fa-chevron-circle-right"></i></a>
+        </div>
+
+        <div
+          class="team-schedule bg-smoke wow fadeInUp"
+          data-wow-delay="0.3s">
+          <h3 class="h4 border-title">Time Schedule</h3>
+          <table class="team-schedule-table">
+            <tbody>
+              <tr>
+                <td>Saturday - Thursday</td>
+                <td>10.00 AM - 3.00 PM</td>
+              </tr>
+              <tr>
+                <td>Room No:</td>
+                <td>N/A</td>
+              </tr>
+
+
+
+            </tbody>
+          </table>
+          <a href="tel:66925682596" class="vs-btn style2">+88 02 8878080<i class="fas fa-phone"></i></a>
+        </div>
+      </div>
+      <div class="col-lg-7">
+        <div class="team-content">
+          <h2 class="mb-0 mt-n2">Prof. Dr. Md. Abdul Jalil Ansari</h2>
+          <h3 class="text-theme fs-xs">Principal<br> Head of Department <br>Department of Endocrinology & Lifestyle Medicine </h3>
+          <p class="fs-md text-title" style="text-align: justify;">
+            <strong>Prof. Dr. Md. Abdul Jalil Ansari</strong> is a senior endocrinologist and medicine specialist with over 35 years with vast experience in treating complex hormonal and metabolic disorders. He is one of the most respected figures in Bangladesh’s endocrinology community and is widely known for his expertise in managing diabetes, thyroid disorders, obesity and hormonal imbalances. His leadership in both clinical practice and medical education has made a lasting impact on the field.
+          </p>
+          <p class="fs-md text-title" style="text-align: justify;">
+            He completed MBBS from Mymensingh Medical College in 1984, MCPS in Medicine from BCPS and MD in Endocrinology & Metabolism from BIRDEM Academy under University of Dhaka. He has held key academic positions in many Govt. & Non-Govt. medical colleges and hospitals in home & abroad, i.e. MMC, BIRDEM, IPGMR, RMC, DMC, SSMC, SOMC, Shahabuddin Medical College, KSA, UMS. currently serves as Princial & Head of the Department of Endocrinology & Lifestyle Medicine at MH Samorita Hospital & Medical College, Dhaka.
+          </p>
+
+          <table class="member-table" style="background-color: #f3f6f7;">
+            <tbody>
+              <tr>
+                <th><strong>Speciality</strong></th>
+                <td>Endocrinology & Metabolism <br> Internal Medicine </td>
+              </tr>
+              <tr>
+                <th><strong>Degrees</strong></th>
+                <td>
+                  <ul class="member-info-list">
+                    <li>MBBS – Mymensingh Medical College, University of Dhaka.</li>
+                    <li>MCPS (Medicine) – BCPS.</li>
+                    <li>MD (Endocrinology & Metabolism) – BIRDEM Academy, University of Dhaka</li>
+              </tr>
+              <tr>
+                <th><strong>Areas of Expertise</strong></th>
+                <td>
+                  <ul class="member-info-list">
+                    <li>Medicine</li>
+                    <li>Type 1 & Type 2 Diabetes</li>
+                    <li>Thyroid disorders (hypo/hyperthyroidism, nodules, goiter)</li>
+                    <li>Obesity & metabolic syndrome</li>
+                    <li>Adrenal & pituitary gland disorders</li>
+                    <li>Hormonal imbalance in men & women</li>
+                    <li>Osteoporosis & bone health</li>
+                    <li>PCOS and menstrual irregularities</li>
+                    <li>Growth and puberty disorders in adolescents</li>
+                    <li>Lipid disorders (cholesterol, triglycerides)</li>
+
+                  </ul>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <form
+            action="#"
+            class="form-wrap1 shadow-none mb-30 rounded-3 overflow-hidden wow fadeInUp"
+            data-wow-delay="0.3s"
+            data-bg-color="#f3f6f7">
+            <div
+              class="form-title-box bg-title"
+              data-bg-src="assets/img/bg/bg-shape-9.jpg">
+              <div class="row justify-content-between align-items-center">
+                <div class="col-xl-10">
+                  <h2 class="h4 mb-2 text-white">Book An Appointment</h2>
+                  <p class="mb-0 text-white-light">
+                    Scenarios and economically sound ROI. Dynamically seize
+                    cross platform "outside the box" thinking after
+                    enterprise...
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="form-box">
+              <div class="row">
+                <div class="col-lg-6 form-group">
+                  <select class="form-select style3">
+                    <option hidden disabled selected>
+                      Select Services
+                    </option>
+                    <option>Aerospace Medicine</option>
+                    <option>Bariatric Surgery</option>
+                    <option>Infectious Diseases</option>
+                    <option>Laboratory Medicine</option>
+                  </select>
+                </div>
+                <div class="col-lg-6 form-group">
+                  <select class="form-select style3">
+                    <option hidden disabled selected>Choose Doctor</option>
+                    <option>Aerospace Medicine</option>
+                    <option>Bariatric Surgery</option>
+                    <option>Infectious Diseases</option>
+                    <option>Laboratory Medicine</option>
+                  </select>
+                </div>
+                <div class="col-lg-6 form-group">
+                  <input
+                    type="text"
+                    class="form-control style3"
+                    placeholder="Your Name" />
+                  <i class="fal small fa-user"></i>
+                </div>
+                <div class="col-lg-6 form-group">
+                  <input
+                    type="email"
+                    class="form-control style3"
+                    placeholder="Email Address" />
+                  <i class="fal small fa-envelope"></i>
+                </div>
+                <div class="col-lg-6 form-group">
+                  <input
+                    type="number"
+                    class="form-control style3"
+                    placeholder="Your Phone" />
+                  <i class="fal small fa-phone"></i>
+                </div>
+                <div class="col-lg-6 form-group">
+                  <input
+                    type="text"
+                    class="dateTime-pick form-control style3"
+                    placeholder="Select Date & Time" />
+                  <i class="fal small fa-calendar-alt"></i>
+                </div>
+                <div class="col-xl-12 text-center">
+                  <button type="submit" class="vs-btn style2">
+                    Make Appointment<i class="far fa-calendar-alt"></i>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </form>
         </div>
       </div>
     </div>
-    <!--==============================
-    Team Details
-    ==============================-->
-    <section class="vs-details-wrapper space-top space-md-bottom">
-      <div class="container">
-        <div class="row gx-40">
-          <div class="col-lg-5">
-            <div
-              class="member-header mb-40 overflow-hidden rounded-3 position-relative"
-            >
-              <div class="member-details-img">
-                
-                <img
-                  src="assets/img/team/abduljalailansari.png"
-                  alt="Member Image"
-                  class="w-100"
-                />
-              </div>
-
-              <div class="member-angle-links">
-                <div class="shape">
-                  <img src="assets/img/bg/member-shape.png" alt="Shape" />
-                </div>
-
-              </div>
-
-              <a href="#" class="certifate-link"
-                ><span style="color: #07ccec; :hover {color: #FFFFFF;}">BMDC Registration Number: A-14668 </span><i
-                  class="far fa-chevron-circle-right"
-                ></i
-              ></a>
-            </div>
-
-            <div
-              class="team-schedule bg-smoke wow fadeInUp"
-              data-wow-delay="0.3s"
-            >
-              <h3 class="h4 border-title">Time Schedule</h3>
-              <table class="team-schedule-table">
-                <tbody>
-                  <tr>
-                    <td>Saturday - Thursday</td>
-                    <td>10.00 AM - 1.00 PM</td>
-                  </tr>
-                  <tr>
-                    <td>Room No:</td>
-                    <td>N/A</td>
-                  </tr>
-                  
-                  
-                
-                </tbody>
-              </table>
-              <a href="tel:66925682596" class="vs-btn style2"
-                >+88 02 8878080<i class="fas fa-phone"></i
-              ></a>
-            </div>
-          </div>
-          <div class="col-lg-7">
-            <div class="team-content">
-              <h2 class="mb-0 mt-n2">Prof. Dr. Md. Abdul Jalil Ansari </h2>
-              <h3 class="text-theme fs-xs">Principal<br> Head of Department <br>Department of Endocrinology </h3>
-              <p class="fs-md text-title" style="text-align: justify;">
-                Prof. Dr. Md. Abdul Jalil Ansari is a senior endocrinologist with over 27 years of experience in treating complex hormonal and metabolic disorders. He is one of the most respected figures in Bangladesh’s endocrinology community and is widely known for his expertise in managing diabetes, thyroid disorders, obesity and hormonal imbalances. His leadership in both clinical practice and medical education has made a lasting impact on the field.
-              </p>
-              <p class="fs-md text-title" style="text-align: justify;">
-               He completed his MBBS from Rangpur Medical College, followed by MCPS in Medicine and MD in Endocrinology & Metabolism from Bangaladesh Medical University (BMU). He has held key academic positions in several major government medical colleges and currently serves as Professor & Head, Department of Endocrinology at Dhaka Medical College & Hospital. He also provides consultations at MH Samorita Hospital.
-              </p>
-              
-              <table class="member-table" style="background-color: #f3f6f7;">
-                <tbody>
-                  <tr>
-                    <th><strong>Speciality</strong></th>
-                    <td>Medicine, Diabetes and Hormone Specialist</td>
-                  </tr>
-                  <tr>
-                    <th><strong>Degrees</strong></th>
-                    <td>
-                      <ul class="member-info-list">
-                        <li>MBBS.</li>
-                        <li>MCPS (Medicine).</li>
-                        <li>MD (Endocrinology & Metabolism)</li>
-                  </tr>
-                  <tr>
-                    <th><strong>Areas of Expertise</strong></th>
-                    <td>
-                      <ul class="member-info-list">
-                            <li>Type 1 & Type 2 Diabetes</li>
-                            <li>Thyroid disorders (hypo/hyperthyroidism, nodules, goiter)</li>
-                            <li>Obesity & metabolic syndrome</li>
-                            <li>Adrenal & pituitary gland disorders</li>
-                            <li>Hormonal imbalance in men & women</li>
-                            <li>Osteoporosis & bone health</li>
-                            <li>PCOS and menstrual irregularities</li>
-                            <li>Growth and puberty disorders in adolescents</li>
-                            <li>Lipid disorders (cholesterol, triglycerides)</li>
-
-                      </ul>
-                    </td>
-                  </tr>
-                  <tr>
-                    <th><strong>Academic Qualification</strong></th>
-                    <td>MBBS from Rangpur Medical College, followed by MCPS in Medicine and MD in Endocrinology & Metabolism from Bangaladesh Medical University (BMU).
-                  </tr>
-                </tbody>
-              </table>
-              <form
-                action="#"
-                class="form-wrap1 shadow-none mb-30 rounded-3 overflow-hidden wow fadeInUp"
-                data-wow-delay="0.3s"
-                data-bg-color="#f3f6f7"
-              >
-                <div
-                  class="form-title-box bg-title"
-                  data-bg-src="assets/img/bg/bg-shape-9.jpg"
-                >
-                  <div class="row justify-content-between align-items-center">
-                    <div class="col-xl-10">
-                      <h2 class="h4 mb-2 text-white">Book An Appointment</h2>
-                      <p class="mb-0 text-white-light">
-                        Scenarios and economically sound ROI. Dynamically seize
-                        cross platform "outside the box" thinking after
-                        enterprise...
-                      </p>
-                    </div>
-                  </div>
-                </div>
-                <div class="form-box">
-                  <div class="row">
-                    <div class="col-lg-6 form-group">
-                      <select class="form-select style3">
-                        <option hidden disabled selected>
-                          Select Services
-                        </option>
-                        <option>Aerospace Medicine</option>
-                        <option>Bariatric Surgery</option>
-                        <option>Infectious Diseases</option>
-                        <option>Laboratory Medicine</option>
-                      </select>
-                    </div>
-                    <div class="col-lg-6 form-group">
-                      <select class="form-select style3">
-                        <option hidden disabled selected>Choose Doctor</option>
-                        <option>Aerospace Medicine</option>
-                        <option>Bariatric Surgery</option>
-                        <option>Infectious Diseases</option>
-                        <option>Laboratory Medicine</option>
-                      </select>
-                    </div>
-                    <div class="col-lg-6 form-group">
-                      <input
-                        type="text"
-                        class="form-control style3"
-                        placeholder="Your Name"
-                      />
-                      <i class="fal small fa-user"></i>
-                    </div>
-                    <div class="col-lg-6 form-group">
-                      <input
-                        type="email"
-                        class="form-control style3"
-                        placeholder="Email Address"
-                      />
-                      <i class="fal small fa-envelope"></i>
-                    </div>
-                    <div class="col-lg-6 form-group">
-                      <input
-                        type="number"
-                        class="form-control style3"
-                        placeholder="Your Phone"
-                      />
-                      <i class="fal small fa-phone"></i>
-                    </div>
-                    <div class="col-lg-6 form-group">
-                      <input
-                        type="text"
-                        class="dateTime-pick form-control style3"
-                        placeholder="Select Date & Time"
-                      />
-                      <i class="fal small fa-calendar-alt"></i>
-                    </div>
-                    <div class="col-xl-12 text-center">
-                      <button type="submit" class="vs-btn style2">
-                        Make Appointment<i class="far fa-calendar-alt"></i>
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              </form>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-    <?php include 'footerhospital.php';?>
+  </div>
+</section>
+<?php include 'footerhospital.php'; ?>
