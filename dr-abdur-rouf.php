93ddf3fcf356d2dafef1f1f1006f30dc5c5c100c
Nayeem Hasan Shanto <snayeemhasan@gmail.com>
2025-08-25 17:54:54 +0600

    dr. abdur rouf's profile updated

--- dr-abdur-rouf.php
+++ dr-abdur-rouf.php
@@ -1,217 +1,119 @@
-<?php include 'headerhospital.php';?>
-    <!--==============================
+<?php include 'headerhospital.php'; ?>
+<!--==============================
     Breadcumb
 ============================== -->
-    <div class="breadcumb-wrapper">
-      <div
-        class="parallax"
-        data-parallax-image="assets/img/breadcurmb/breadcurmb-1-1.jpg"
-      ></div>
-      <div class="container z-index-common">
-        <div class="breadcumb-content">
-          <h1 class="breadcumb-title" style="color: whitesmoke;"></h1>
-          <div class="breadcumb-menu-wrap">
-            <i class="far fa-home-lg"></i>
-            <ul class="breadcumb-menu">
-              <li><a href="index-4.php">Home</a></li>
-              <li><a href="alldoctor.php">Doctor Details</a></li>
-              
-            </ul>
-          </div>
-        </div>
+<div class="breadcumb-wrapper">
+  <div
+    class="parallax"
+    data-parallax-image="assets/img/breadcurmb/breadcurmb-1-1.jpg"></div>
+  <div class="container z-index-common">
+    <div class="breadcumb-content">
+      <h1 class="breadcumb-title">Department of Paediatrics</h1>
+      <div class="breadcumb-menu-wrap">
+        <i class="far fa-home-lg"></i>
+        <ul class="breadcumb-menu">
+          <li><a href="index-4.php">Home</a></li>
+          <li><a href="alldoctor.php">Doctor Details</a></li>
+
+        </ul>
       </div>
     </div>
-    <!--==============================
+  </div>
+</div>
+<!--==============================
     Team Details
     ==============================-->
-    <section class="vs-details-wrapper space-top space-md-bottom">
-      <div class="container">
-        <div class="row gx-40">
-          <div class="col-lg-5">
-            <div
-              class="member-header mb-40 overflow-hidden rounded-3 position-relative"
-            >
-              <div class="member-details-img">
-                <img
-                  src="assets/img/team/avatar-details-profile.png"
-                  alt="Member Image"
-                  class="w-100"
-                />
-                
-              </div>
+<section class="vs-details-wrapper space-top space-md-bottom">
+  <div class="container">
+    <div class="row gx-40">
+      <div class="col-lg-5">
+        <div
+          class="member-header mb-40 overflow-hidden rounded-3 position-relative">
+          <div class="member-details-img">
+            <img
+              src="assets/img/team/avatar-details-profile.png"
+              alt="Member Image"
+              class="w-100" />
 
-              <div class="member-angle-links">
-                <div class="shape">
-                  <img src="assets/img/bg/member-shape.png" alt="Shape" />
-                </div>
-
-              </div>
-
-              <a href="https://verify.bmdc.org.bd/" class="certifate-link"
-                ><span style="color: #07ccec; :hover {color: #FFFFFF;}">BMDC Registration Number: A-17037 </span><i
-                  class="far fa-chevron-circle-right"
-                ></i
-              ></a>
-            </div>
-
-            <div
-              class="team-schedule bg-smoke wow fadeInUp"
-              data-wow-delay="0.3s"
-            >
-              <h3 class="h4 border-title">Time Schedule</h3>
-              <table class="team-schedule-table">
-                <tbody>
-                  <tr>
-                    <td>Saturday - Thursday</td>
-                    <td>10.00 AM - 1.00 PM</td>
-                  </tr>
-                  <tr>
-                    <td>Room No:</td>
-                    <td>N/A</td>
-                  </tr>
-                  
-                  
-                
-                </tbody>
-              </table>
-              <a href="tel:66925682596" class="vs-btn style2"
-                >+88 02 8878080<i class="fas fa-phone"></i
-              ></a>
-            </div>
           </div>
-          <div class="col-lg-7">
-            <div class="team-content">
-              <h2 class="mb-0 mt-n2">Prof. Dr. Md. Abdur Rouf</h2>
-              <h3 class="text-theme fs-xs">Professor & Head<br> Department of Paediatrics</h3>
-              <p class="fs-md text-title" style="text-align: justify;">
-                Prof. Dr. Md. Abdur Rouf is a senior and highly esteemed paediatrician with more than 30 years of experience in child health, neonatology and paediatric education. As a Professor in the Department of Paediatrics at MH Samorita Hospital & Medical College, he plays a pivotal role in delivering compassionate care and mentoring future doctors. He is known for his diagnostic precision, clinical leadership and patient-friendly approach, especially in managing complex paediatric and neonatal cases.
-              </p>
-        
-              
-              <table class="member-table" style="background-color: #f3f6f7;">
-                <tbody>
-                  <tr>
-                    <th><strong>Speciality</strong></th>
-                    <td>Paediatrics & Neonatology</td>
-                  </tr>
-                  <tr>
-                    <th><strong>Degrees</strong></th>
-                    <td>
-                      <ul class="member-info-list">
-                    
-                        <li>MBBS.</li>
-                        <li>DCH.</li>
-                        <li>FCPS (Paediatrics).</li>
-                        <li>MD (Paediatrics)</li>
-                  </tr>
-                  <tr>
-                    <th><strong>Areas of Expertise</strong></th>
-                    <td>
-                      <ul class="member-info-list">
-                                <li>Newborn care and neonatal infections</li>
-                                <li>Childhood asthma, pneumonia and respiratory illness</li>
-                                <li>Paediatric gastroenterology and nutrition disorders</li>
-                                <li>Vaccination and immunization planning</li>
-                                <li>Growth, development and behavioral assessment</li>
-                                <li>Management of congenital disorders</li>
-                                <li>Paediatric emergency and critical care</li>
-                                <li>Allergy, skin problems and fever management in children</li>
-                                <li>School-age health screening and well-child visits</li>
-                      </ul>
-                    </td>
-                  </tr>
-                  <tr>
-                    <th><strong>Academic Qualification</strong></th>
-                    <td>MBBS from Dhaka Medical College, DCH (Diploma in Child Health) from BMU, FCPS (Paediatrics) from the Bangladesh College of Physicians and Surgeons, and MD (Paediatrics) from Bangladesh Medical University (BMU).</td>
-                  </tr>
-                </tbody>
-              </table>
-              <form
-                action="#"
-                class="form-wrap1 shadow-none mb-30 rounded-3 overflow-hidden wow fadeInUp"
-                data-wow-delay="0.3s"
-                data-bg-color="#f3f6f7"
-              >
-                <!-- <div
-                  class="form-title-box bg-title"
-                  data-bg-src="assets/img/bg/bg-shape-9.jpg"
-                >
-                  <div class="row justify-content-between align-items-center">
-                    <div class="col-xl-10">
-                      <h2 class="h4 mb-2 text-white">Book An Appointment</h2>
-                      <p class="mb-0 text-white-light">
-                        Scenarios and economically sound ROI. Dynamically seize
-                        cross platform "outside the box" thinking after
-                        enterprise...
-                      </p>
-                    </div>
-                  </div>
-                </div>
-                <div class="form-box">
-                  <div class="row">
-                    <div class="col-lg-6 form-group">
-                      <select class="form-select style3">
-                        <option hidden disabled selected>
-                          Select Services
-                        </option>
-                        <option>Aerospace Medicine</option>
-                        <option>Bariatric Surgery</option>
-                        <option>Infectious Diseases</option>
-                        <option>Laboratory Medicine</option>
-                      </select>
-                    </div>
-                    <div class="col-lg-6 form-group">
-                      <select class="form-select style3">
-                        <option hidden disabled selected>Choose Doctor</option>
-                        <option>Aerospace Medicine</option>
-                        <option>Bariatric Surgery</option>
-                        <option>Infectious Diseases</option>
-                        <option>Laboratory Medicine</option>
-                      </select>
-                    </div>
-                    <div class="col-lg-6 form-group">
-                      <input
-                        type="text"
-                        class="form-control style3"
-                        placeholder="Your Name"
-                      />
-                      <i class="fal small fa-user"></i>
-                    </div>
-                    <div class="col-lg-6 form-group">
-                      <input
-                        type="email"
-                        class="form-control style3"
-                        placeholder="Email Address"
-                      />
-                      <i class="fal small fa-envelope"></i>
-                    </div>
-                    <div class="col-lg-6 form-group">
-                      <input
-                        type="number"
-                        class="form-control style3"
-                        placeholder="Your Phone"
-                      />
-                      <i class="fal small fa-phone"></i>
-                    </div>
-                    <div class="col-lg-6 form-group">
-                      <input
-                        type="text"
-                        class="dateTime-pick form-control style3"
-                        placeholder="Select Date & Time"
-                      />
-                      <i class="fal small fa-calendar-alt"></i>
-                    </div>
-                    <div class="col-xl-12 text-center">
-                      <button type="submit" class="vs-btn style2">
-                        Make Appointment<i class="far fa-calendar-alt"></i>
-                      </button>
-                    </div>
-                  </div> -->
-                </div>
-              </form>
+
+          <div class="member-angle-links">
+            <div class="shape">
+              <img src="assets/img/bg/member-shape.png" alt="Shape" />
             </div>
+
           </div>
+
+          <a href="https://verify.bmdc.org.bd/" class="certifate-link"><span style="color: #07ccec; :hover {color: #FFFFFF;}">BMDC Registration Number: A-17037 </span><i
+              class="far fa-chevron-circle-right"></i></a>
+        </div>
+
+        <div
+          class="team-schedule bg-smoke wow fadeInUp"
+          data-wow-delay="0.3s">
+          <h3 class="h4 border-title">Time Schedule</h3>
+          <table class="team-schedule-table">
+            <tbody>
+              <tr>
+                <td>Saturday - Thursday</td>
+                <td>10.00 AM - 1.00 PM</td>
+              </tr>
+              <tr>
+                <td>Room No:</td>
+                <td>806</td>
+              </tr>
+
+
+
+            </tbody>
+          </table>
+          <a href="tel:66925682596" class="vs-btn style2">+88 02 8878080<i class="fas fa-phone"></i></a>
         </div>
       </div>
-    </section>
-    <?php include 'footerhospital.php';?>
+      <div class="col-lg-7">
+        <div class="team-content">
+          <h2 class="mb-0 mt-n2">Prof. Dr. Md. Abdur Rouf</h2>
+          <h3 class="text-theme fs-xs">MBBS, DCH, FCPS (Paediatrics), MD (Paediatrics) <br><br>Professor <br> Head of Department <br> Department of Paediatrics</h3>
+          <p class="fs-md text-title" style="text-align: justify;">
+            <strong>Prof. Dr. Md. Abdur Rouf</strong> is a senior and highly esteemed paediatrician with more than 30 years of experience in child health, neonatology and paediatric education. He has joined to Bangaldesh Govt. Service in 1989 through 8th BCS and then worked in different hospitals of home and abroad. He retired from Govt. Service in March, 2022 as Professor & Head of the Department of Paediatrics of Sir Salimullah Medical College & Hospital, Mitford, Dhaka. Then he serves as a Professor & Head of the Department of Paediatrics at MH Samorita Hospital & Medical College, he plays a pivotal role in delivering compassionate care and mentoring future doctors. He is known for his diagnostic precision, clinical leadership and patient-friendly approach, especially in managing complex paediatric and neonatal cases. He is also the member of European Society of Paediatric Infectious Disease (ESPID), Mentor for Postgraduate Thesis Work and Examiner of Bangladesh College of Physicians & Surgeons (BCPS), Bangladesh Medical University (BMU) and University of Dhaka.
+          </p>
+
+
+          <table class="member-table" style="background-color: #f3f6f7;">
+            <tbody>
+              <tr>
+                <th><strong>Speciality</strong></th>
+                <td>Infection disease, Respiratory Medicine & Neonatology</td>
+              </tr>
+              <tr>
+                <th><strong>Degrees</strong></th>
+                <td>
+                  <ul class="member-info-list">
+
+                    <li>MBBS – Sir Salimullah Medical College, University of Dhaka</li>
+                    <li>FCPS (Paediatrics) – Bangladesh College of Physicians & Surgeons (BCPS)</li>
+              </tr>
+              <tr>
+                <th><strong>Areas of Expertise</strong></th>
+                <td>
+                  <ul class="member-info-list">
+                    <li>Newborn care and neonatal infections</li>
+                    <li>Childhood asthma, pneumonia and respiratory illness</li>
+                    <li>Paediatric gastroenterology and nutrition disorders</li>
+                    <li>Vaccination and immunization planning</li>
+                    <li>Growth, development and behavioral assessment</li>
+                    <li>Management of congenital disorders</li>
+                    <li>Paediatric emergency and critical care</li>
+                    <li>Allergy, skin problems and fever management in children</li>
+                    <li>School-age health screening and well-child visits</li>
+                  </ul>
+                </td>
+              </tr>             
+            </tbody>
+          </table>
+      </div>
+    </div>
+  </div>
+  </div>
+</section>
+<?php include 'footerhospital.php'; ?>
