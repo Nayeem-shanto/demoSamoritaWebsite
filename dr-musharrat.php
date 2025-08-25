2f1d09ae4fb0637c6eeea060772310b39f40c643
Nayeem Hasan Shanto <snayeemhasan@gmail.com>
2025-08-25 18:21:25 +0600

    dr. musharrat's profile updated

--- dr-musharrat.php
+++ dr-musharrat.php
@@ -1,220 +1,117 @@
-<?php include 'headerhospital.php';?>
-    <!--==============================
+<?php include 'headerhospital.php'; ?>
+<!--==============================
     Breadcumb
 ============================== -->
-    <div class="breadcumb-wrapper">
-      <div
-        class="parallax"
-        data-parallax-image="assets/img/breadcurmb/breadcurmb-1-1.jpg"
-      ></div>
-      <div class="container z-index-common">
-        <div class="breadcumb-content">
-          <h1 class="breadcumb-title" style="color: whitesmoke;"></h1>
-          <div class="breadcumb-menu-wrap">
-            <i class="far fa-home-lg"></i>
-            <ul class="breadcumb-menu">
-              <li><a href="index-4.php">Home</a></li>
-              <li><a href="alldoctor.php">Doctor Details</a></li>
-              
-            </ul>
+<div class="breadcumb-wrapper">
+  <div
+    class="parallax"
+    data-parallax-image="assets/img/breadcurmb/breadcurmb-1-1.jpg"></div>
+  <div class="container z-index-common">
+    <div class="breadcumb-content">
+      <h1 class="breadcumb-title">Department of Paediatrics</h1>
+      <div class="breadcumb-menu-wrap">
+        <i class="far fa-home-lg"></i>
+        <ul class="breadcumb-menu">
+          <li><a href="index-4.php">Home</a></li>
+          <li><a href="alldoctor.php">Doctor Details</a></li>
+
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
+<!--==============================
+    Team Details
+    ==============================-->
+<section class="vs-details-wrapper space-top space-md-bottom">
+  <div class="container">
+    <div class="row gx-40">
+      <div class="col-lg-5">
+        <div
+          class="member-header mb-40 overflow-hidden rounded-3 position-relative">
+          <div class="member-details-img">
+            <img
+              src="assets/img/team/female-details.jpg"
+              alt="Member Image"
+              class="w-100" />
           </div>
+
+          <div class="member-angle-links">
+            <div class="shape">
+              <img src="assets/img/bg/member-shape.png" alt="Shape" />
+            </div>
+
+          </div>
+
+          <a href="#" class="certifate-link"><span style="color: #07ccec; :hover {color: #FFFFFF;}">BMDC Registration Number: A-123733 </span><i
+              class="far fa-chevron-circle-right"></i></a>
+        </div>
+
+        <div
+          class="team-schedule bg-smoke wow fadeInUp"
+          data-wow-delay="0.3s">
+          <h3 class="h4 border-title">Time Schedule</h3>
+          <table class="team-schedule-table">
+            <tbody>
+              <tr>
+                <td>Saturday - Thursday</td>
+                <td>9.00 AM - 2.00 PM</td>
+              </tr>
+              <tr>
+                <td>Room No:</td>
+                <td>805</td>
+              </tr>
+
+
+
+            </tbody>
+          </table>
+          <a href="tel:66925682596" class="vs-btn style2">+88 02 8878080<i class="fas fa-phone"></i></a>
+        </div>
+      </div>
+      <div class="col-lg-7">
+        <div class="team-content">
+          <h2 class="mb-0 mt-n2">Dr. Musharrat Tasnim</h2>
+          <h3 class="text-theme fs-xs">MBBS<br><br>Assistant Registrar<br> Department of Paediatrics</h3>
+          <p class="fs-md text-title" style="text-align: justify;">
+            <strong>Dr. Musharrat Tasnim</strong> is a dedicated and compassionate physician specializing in paediatrics, currently serving as an Assistant Registrar in the Department of Paediatrics at MH Samorita Hospital & Medical College. With a strong foundation in paediatric clinical care, she is actively involved in both inpatient and outpatient services, providing holistic child healthcare. Her professional approach blends medical expertise with empathetic interaction, ensuring comfort and trust for children and their families. She is also committed to continuous learning, research and medical education.
+          </p>
+
+
+          <table class="member-table" style="background-color: #f3f6f7;">
+            <tbody>
+              <tr>
+                <th><strong>Specialty</strong></th>
+                <td>General Paediatrics <br>Child growth and development monitoring <br>Preventive child health and immunization programs </td>
+              </tr>
+              <tr>
+                <th><strong>Degrees</strong></th>
+                <td>
+                  <ul class="member-info-list">
+                    <li>MBBS – MH Samorita Hospital & Medical College, University of Dhaka</li>
+                    <li>Experienced in ward-based teaching, paediatric audits and continuing medical education</li>
+              </tr>
+              <tr>
+                <th><strong>Areas of Expertise</strong></th>
+                <td>
+                  <ul class="member-info-list">
+                    <li>Comprehensive paediatric assessment and follow-up care</li>
+                    <li>Management of Fever, respiratory infections and diarrheal diseases</li>
+                    <li>Growth monitoring and nutritional counselling</li>
+                    <li>Neonatal care, screening and early intervention</li>
+                    <li>Immunization programs and parental health education</li>
+                    <li>Ward coordination and discharge planning</li>
+                    <li>Emergency paediatric care support</li>
+                    <li>Academic rounds, clinical teaching and student mentoring</li>
+
+                  </ul>
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
-    <!--==============================
-    Team Details
-    ==============================-->
-    <section class="vs-details-wrapper space-top space-md-bottom">
-      <div class="container">
-        <div class="row gx-40">
-          <div class="col-lg-5">
-            <div
-              class="member-header mb-40 overflow-hidden rounded-3 position-relative"
-            >
-              <div class="member-details-img">
-                <!-- <img
-                  src="assets/img/team/avatar-details-profile.png"
-                  alt="Member Image"
-                  class="w-100"
-                /> -->
-
-                <img
-                  src="assets/img/team/female-details.jpg"
-                  alt="Member Image"
-                  class="w-100"
-                />
-                
-              </div>
-
-              <div class="member-angle-links">
-                <div class="shape">
-                  <img src="assets/img/bg/member-shape.png" alt="Shape" />
-                </div>
-
-              </div>
-
-              <a href="https://verify.bmdc.org.bd/" class="certifate-link"
-                ><span style="color: #07ccec; :hover {color: #FFFFFF;}">BMDC Registration Number: A-123733 </span><i
-                  class="far fa-chevron-circle-right"
-                ></i
-              ></a>
-            </div>
-
-            <div
-              class="team-schedule bg-smoke wow fadeInUp"
-              data-wow-delay="0.3s"
-            >
-              <h3 class="h4 border-title">Time Schedule</h3>
-              <table class="team-schedule-table">
-                <tbody>
-                  <tr>
-                    <td>Saturday - Thursday</td>
-                    <td>9.00 AM - 2.00 PM</td>
-                  </tr>
-                  <tr>
-                    <td>Room No:</td>
-                    <td>N/A</td>
-                  </tr>
-                  
-                  
-                
-                </tbody>
-              </table>
-              <a href="tel:66925682596" class="vs-btn style2"
-                >+88 02 8878080<i class="fas fa-phone"></i
-              ></a>
-            </div>
-          </div>
-          <div class="col-lg-7">
-            <div class="team-content">
-              <h2 class="mb-0 mt-n2">Dr. Musharrat Tasnim</h2>
-              <h3 class="text-theme fs-xs">Assistant Registar<br> Department of Paediatrics</h3>
-              <p class="fs-md text-title" style="text-align: justify;">
-                Dr. Musharrat Tasnim is a dedicated and compassionate physician working as an Assistant Registrar in the Department of Paediatrics at MH Samorita Hospital & Medical College. With a solid foundation in paediatric clinical care, she supports both inpatient and outpatient services and assists in academic and ward-based teaching. She is known for her empathetic interaction with children and families and for maintaining a disciplined approach to child health management.
-              </p>
-        
-              
-              <table class="member-table" style="background-color: #f3f6f7;">
-                <tbody>
-                  <tr>
-                    <th><strong>Speciality</strong></th>
-                    <td>General Paediatrics</td>
-                  </tr>
-                  <tr>
-                    <th><strong>Degrees</strong></th>
-                    <td>
-                      <ul class="member-info-list">
-                    
-                        <li>MBBS.</li>
-
-                  </tr>
-                  <tr>
-                    <th><strong>Areas of Expertise</strong></th>
-                    <td>
-                      <ul class="member-info-list">
-                            <li>General paediatric assessment and follow-up</li>
-                            <li>Fever, respiratory infections and diarrheal diseases</li>
-                            <li>Growth monitoring and nutritional counselling</li>
-                            <li>Neonatal care and screening</li>
-                            <li>Immunization and health education for parents</li>
-                            <li>Ward coordination and discharge planning</li>
-                            <li>Support in emergency paediatric care</li>
-                            <li>Assisting in academic rounds and clinical teaching</li>
-                      </ul>
-                    </td>
-                  </tr>
-                  <tr>
-                    <th><strong>Academic Qualification</strong></th>
-                    <td>MBBS from a reputed medical college under the University of Dhaka, with postgraduate training in Paediatrics currently ongoing.</td>
-                  </tr>
-                </tbody>
-              </table>
-              <form
-                action="#"
-                class="form-wrap1 shadow-none mb-30 rounded-3 overflow-hidden wow fadeInUp"
-                data-wow-delay="0.3s"
-                data-bg-color="#f3f6f7"
-              >
-                <!-- <div
-                  class="form-title-box bg-title"
-                  data-bg-src="assets/img/bg/bg-shape-9.jpg"
-                >
-                  <div class="row justify-content-between align-items-center">
-                    <div class="col-xl-10">
-                      <h2 class="h4 mb-2 text-white">Book An Appointment</h2>
-                      <p class="mb-0 text-white-light">
-                        Scenarios and economically sound ROI. Dynamically seize
-                        cross platform "outside the box" thinking after
-                        enterprise...
-                      </p>
-                    </div>
-                  </div>
-                </div>
-                <div class="form-box">
-                  <div class="row">
-                    <div class="col-lg-6 form-group">
-                      <select class="form-select style3">
-                        <option hidden disabled selected>
-                          Select Services
-                        </option>
-                        <option>Aerospace Medicine</option>
-                        <option>Bariatric Surgery</option>
-                        <option>Infectious Diseases</option>
-                        <option>Laboratory Medicine</option>
-                      </select>
-                    </div>
-                    <div class="col-lg-6 form-group">
-                      <select class="form-select style3">
-                        <option hidden disabled selected>Choose Doctor</option>
-                        <option>Aerospace Medicine</option>
-                        <option>Bariatric Surgery</option>
-                        <option>Infectious Diseases</option>
-                        <option>Laboratory Medicine</option>
-                      </select>
-                    </div>
-                    <div class="col-lg-6 form-group">
-                      <input
-                        type="text"
-                        class="form-control style3"
-                        placeholder="Your Name"
-                      />
-                      <i class="fal small fa-user"></i>
-                    </div>
-                    <div class="col-lg-6 form-group">
-                      <input
-                        type="email"
-                        class="form-control style3"
-                        placeholder="Email Address"
-                      />
-                      <i class="fal small fa-envelope"></i>
-                    </div>
-                    <div class="col-lg-6 form-group">
-                      <input
-                        type="number"
-                        class="form-control style3"
-                        placeholder="Your Phone"
-                      />
-                      <i class="fal small fa-phone"></i>
-                    </div>
-                    <div class="col-lg-6 form-group">
-                      <input
-                        type="text"
-                        class="dateTime-pick form-control style3"
-                        placeholder="Select Date & Time"
-                      />
-                      <i class="fal small fa-calendar-alt"></i>
-                    </div>
-                    <div class="col-xl-12 text-center">
-                      <button type="submit" class="vs-btn style2">
-                        Make Appointment<i class="far fa-calendar-alt"></i>
-                      </button>
-                    </div>
-                  </div> -->
-                </div>
-              </form>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-    <?php include 'footerhospital.php';?>
+  </div>
+</section>
+<?php include 'footerhospital.php'; ?>
