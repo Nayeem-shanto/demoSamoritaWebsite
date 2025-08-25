e49987c508eef5dc0a1b8a5cd45a69144905c96e
Nayeem Hasan Shanto <snayeemhasan@gmail.com>
2025-08-25 17:15:22 +0600

    dr. tamanna ahmed

---
+++ dr-tamanna-ahmed.php
@@ -0,0 +1,206 @@
+<?php include 'headerhospital.php'; ?>
+<!--==============================
+    Breadcumb
+============================== -->
+<div class="breadcumb-wrapper">
+    <div
+        class="parallax"
+        data-parallax-image="assets/img/breadcurmb/breadcurmb-1-1.jpg"></div>
+    <div class="container z-index-common">
+        <div class="breadcumb-content">
+            <h1 class="breadcumb-title">Department of Surgery</h1>
+            <div class="breadcumb-menu-wrap">
+                <i class="far fa-home-lg"></i>
+                <ul class="breadcumb-menu">
+                    <li><a href="index-4.php">Home</a></li>
+                    <li><a href="alldoctor.php">Doctor Details</a></li>
+
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+<!--==============================
+    Team Details
+    ==============================-->
+<section class="vs-details-wrapper space-top space-md-bottom">
+    <div class="container">
+        <div class="row gx-40">
+            <div class="col-lg-5">
+                <div
+                    class="member-header mb-40 overflow-hidden rounded-3 position-relative">
+                    <div class="member-details-img">
+                        <img
+                            src="assets/img/team/female-details.jpg"
+                            alt="Member Image"
+                            class="w-100" />
+                        <!-- <img
+                  src="assets/img/team/avatar-details-profile.png"
+                  alt="Member Image"
+                  class="w-100"
+                /> -->
+                    </div>
+
+                    <div class="member-angle-links">
+                        <div class="shape">
+                            <img src="assets/img/bg/member-shape.png" alt="Shape" />
+                        </div>
+
+                    </div>
+
+                    <a href="#" class="certifate-link"><span style="color: #07ccec; :hover {color: #FFFFFF;}">BMDC Registration Number: A-64945 </span><i
+                            class="far fa-chevron-circle-right"></i></a>
+                </div>
+
+                <div
+                    class="team-schedule bg-smoke wow fadeInUp"
+                    data-wow-delay="0.3s">
+                    <h3 class="h4 border-title">Time Schedule</h3>
+                    <table class="team-schedule-table">
+                        <tbody>
+                            <tr>
+                                <td>Saturday - Thursday</td>
+                                <td>9.00 AM - 1.00 PM</td>
+                            </tr>
+                            <tr>
+                                <td>Room No:</td>
+                                <td>209</td>
+                            </tr>
+
+
+
+                        </tbody>
+                    </table>
+                    <a href="tel:66925682596" class="vs-btn style2">+88 02 8878080<i class="fas fa-phone"></i></a>
+                </div>
+            </div>
+            <div class="col-lg-7">
+                <div class="team-content">
+                    <h2 class="mb-0 mt-n2">Asst. Prof. Dr. Tamanna Ahmed</h2>
+                    <h3 class="text-theme fs-xs">MBBS, MS (Plastic Surgery) <br><br> Assistant Professor<br> Plastic Surgery Unit, Department of Surgery</h3>
+                    <p class="fs-md text-title" style="text-align: justify;">
+                        <strong>Asst. Prof. Dr. Tamanna Ahmed</strong> is a skilled plastic surgeon currently serving as Assistant Professor in the Plastic Surgery Unit, Department of Surgery at MH Samorita Hospital & Medical College. She is actively involved in both surgical practice and academic instruction. Dr. Tamanna is known for her precision in surgical procedures, dedication to patient care and commitment to training the next generation of surgeons. Her clinical expertise covers a wide range of plastic and reconstructive surgeries with emphasis on patient safety and post-operative recovery.
+                    </p>
+
+
+                    <table class="member-table" style="background-color: #f3f6f7;">
+                        <tbody>
+                            <tr>
+                                <th><strong>Specialty</strong></th>
+                                <td>Plastic and Reconstructive Surgery</td>
+                            </tr>
+                            <tr>
+                                <th><strong>Degrees</strong></th>
+                                <td>
+                                    <ul class="member-info-list">
+                                        <li>MBBS – Rajshahi Medical College, University of Rajshahi</li>
+                                        <li>MS (Plastic Surgery) – National Institute of Burn & Plastic Surgery, BMU</li>
+                            </tr>
+                            <tr>
+                                <th><strong>Areas of Expertise</strong></th>
+                                <td>
+                                    <ul class="member-info-list">
+                                        <li>Plastic and Reconstructive surgery</li>
+                                        <li>Breast surgery</li>
+                                        <li>Aesthetic surgery</li>
+                                        <li>Hand surgery</li>
+                                        <li>Congenital & Craniofacial surgery</li>
+                                        <li>Emergency and trauma surgery</li>
+                                        <li>Burn & Post-burn rehabilitation</li>
+                                        <li>Trauma & Reconstruction</li>
+                                        <li>Post-operative care and counseling</li>
+                                        <li>Minor surgical interventions</li>
+
+                                    </ul>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <form
+                        action="#"
+                        class="form-wrap1 shadow-none mb-30 rounded-3 overflow-hidden wow fadeInUp"
+                        data-wow-delay="0.3s"
+                        data-bg-color="#f3f6f7">
+                        <!-- <div
+                  class="form-title-box bg-title"
+                  data-bg-src="assets/img/bg/bg-shape-9.jpg"
+                >
+                  <div class="row justify-content-between align-items-center">
+                    <div class="col-xl-10">
+                      <h2 class="h4 mb-2 text-white">Book An Appointment</h2>
+                      <p class="mb-0 text-white-light">
+                        Scenarios and economically sound ROI. Dynamically seize
+                        cross platform "outside the box" thinking after
+                        enterprise...
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div class="form-box">
+                  <div class="row">
+                    <div class="col-lg-6 form-group">
+                      <select class="form-select style3">
+                        <option hidden disabled selected>
+                          Select Services
+                        </option>
+                        <option>Aerospace Medicine</option>
+                        <option>Bariatric Surgery</option>
+                        <option>Infectious Diseases</option>
+                        <option>Laboratory Medicine</option>
+                      </select>
+                    </div>
+                    <div class="col-lg-6 form-group">
+                      <select class="form-select style3">
+                        <option hidden disabled selected>Choose Doctor</option>
+                        <option>Aerospace Medicine</option>
+                        <option>Bariatric Surgery</option>
+                        <option>Infectious Diseases</option>
+                        <option>Laboratory Medicine</option>
+                      </select>
+                    </div>
+                    <div class="col-lg-6 form-group">
+                      <input
+                        type="text"
+                        class="form-control style3"
+                        placeholder="Your Name"
+                      />
+                      <i class="fal small fa-user"></i>
+                    </div>
+                    <div class="col-lg-6 form-group">
+                      <input
+                        type="email"
+                        class="form-control style3"
+                        placeholder="Email Address"
+                      />
+                      <i class="fal small fa-envelope"></i>
+                    </div>
+                    <div class="col-lg-6 form-group">
+                      <input
+                        type="number"
+                        class="form-control style3"
+                        placeholder="Your Phone"
+                      />
+                      <i class="fal small fa-phone"></i>
+                    </div>
+                    <div class="col-lg-6 form-group">
+                      <input
+                        type="text"
+                        class="dateTime-pick form-control style3"
+                        placeholder="Select Date & Time"
+                      />
+                      <i class="fal small fa-calendar-alt"></i>
+                    </div>
+                    <div class="col-xl-12 text-center">
+                      <button type="submit" class="vs-btn style2">
+                        Make Appointment<i class="far fa-calendar-alt"></i>
+                      </button>
+                    </div>
+                  </div> -->
+                </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    </div>
+</section>
+<?php include 'footerhospital.php'; ?>
