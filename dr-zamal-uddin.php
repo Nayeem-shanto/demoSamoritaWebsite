62f3dc0d6911a681eabec84c0231cb9380cf9920
Nayeem Hasan Shanto <snayeemhasan@gmail.com>
2025-08-25 17:23:50 +0600

    dr. zamal uddin's profile added

---
+++ dr-zamal-uddin.php
@@ -0,0 +1,204 @@
+<?php include 'headerhospital.php'; ?>
+<!--==============================
+    Breadcumb
+============================== -->
+<div class="breadcumb-wrapper">
+    <div
+        class="parallax"
+        data-parallax-image="assets/img/breadcurmb/breadcurmb-1-1.jpg"></div>
+    <div class="container z-index-common">
+        <div class="breadcumb-content">
+            <h1 class="breadcumb-title">Department of Surgery</h1>
+            <div class="breadcumb-menu-wrap">
+                <i class="far fa-home-lg"></i>
+                <ul class="breadcumb-menu">
+                    <li><a href="index.php">Home</a></li>
+                    <li class="active">Doctors Details</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+<!--==============================
+    Team Details
+    ==============================-->
+<section class="vs-details-wrapper space-top space-md-bottom">
+    <div class="container">
+        <div class="row gx-40">
+            <div class="col-lg-5">
+                <div
+                    class="member-header mb-40 overflow-hidden rounded-3 position-relative">
+                    <div class="member-details-img">
+                        <img
+                            src="./assets/img/team/avatar-details-profile.png"
+                            alt="Member Image"
+                            class="w-100" />
+                    </div>
+
+                    <div class="member-angle-links">
+                        <div class="shape">
+                            <img src="assets/img/bg/member-shape.png" alt="Shape" />
+                        </div>
+                        <span class="middle-icon">
+                            <!-- <i class="fas fa-share-alt"></i> -->
+                        </span>
+                        <!-- <a href="#"><i class="fab fa-facebook-f"></i></a>
+                <a href="#"><i class="fab fa-twitter"></i></a>
+                <a href="#"><i class="fab fa-google"></i></a>
+                <a href="#"><i class="fab fa-linkedin-in"></i></a> -->
+                    </div>
+
+                    <a href="https://verify.bmdc.org.bd/" class="certifate-link"><span style="color: #07ccec; :hover {color: #FFFFFF;}">BMDC Registration Number: A- 34055 </span>
+                        <i class="far fa-chevron-circle-right"></i></a>
+                </div>
+
+                <div
+                    class="team-schedule bg-smoke wow fadeInUp"
+                    data-wow-delay="0.3s">
+                    <h3 class="h4 border-title">Time Schedule</h3>
+                    <table class="team-schedule-table">
+                        <tbody>
+                            <tr>
+                                <td>Saturday - Thursday:</td>
+                                <td>11.00 am - 12.30 pm</td>
+                            </tr>
+                            <tr>
+                                <td>Room No:</td>
+                                <td>209</td>
+                            </tr>
+
+                        </tbody>
+                    </table>
+                    <a href="tel:66925682596" class="vs-btn style2">+88 02 8878080<i class="fas fa-phone"></i></a>
+                </div>
+            </div>
+            <div class="col-lg-7">
+                <div class="team-content">
+                    <h2 class="mb-0 mt-n2">Asst. Prof. Dr. Zamal Uddin</h2>
+                    <p class="text-theme fs-xs">MBBS, FCPS (Surgery) <br><br> Assistant Professor<br>Department of Surgery</p>
+                    <div style="text-align: justify;">
+                        <p class="fs-md text-title">
+                            <strong>Asst. Prof. Dr. Zamal Uddin</strong> is a committed general surgeon currently serving as Assistant Professor in the Department of Surgery at MH Samorita Hospital & Medical College. He is actively engaged in both academic responsibilities and surgical patient care. Dr. Zamal is known for his surgical precision, sound clinical judgment and focus on safe and effective treatment outcomes. He is experienced in handling a wide range of general and emergency surgical cases with a structured, patient-centered approach.
+                        </p>
+                    </div>
+                    <table class="member-table">
+                        <tbody style="background-color: #f3f6f7;">
+                            <tr>
+                                <th><strong>Specialty</strong></th>
+                                <td> General & Laparoscopic Surgery</td>
+                            </tr>
+                            <tr>
+                                <th><strong>Degrees</strong></th>
+                                <td>
+                                    <ul class="member-info-list">
+                                        <li>MBBS – M. Abdur Rahim Medical Collge,Dinajpur, University of Rajshahi</li>
+                                        <li>FCPS (Surgery) – Bangladesh College of Physicians & Surgeons (BCPS)</li>
+                                    </ul>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th><strong>Areas of Expertise</strong></th>
+                                <td>
+                                    <ul class="member-info-list">
+                                        <li>General & Laparoscopic surgery</li>
+                                        <li>Gallbladder and appendix surgery</li>
+                                        <li>Hernia surgery</li>
+                                        <li>Gastrointestinal and colorectal surgery</li>
+                                        <li>Breast surgery</li>
+                                        <li>Minor and emergency surgical care</li>
+                                        <li>Trauma surgery</li>
+                                        <li>Post-operative follow-up and care</li>
+                                        <li>Colorectal, Laser, Longo – Surgery</li>
+                                    </ul>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <form
+                        action="#"
+                        class="form-wrap1 shadow-none mb-30 rounded-3 overflow-hidden wow fadeInUp"
+                        data-wow-delay="0.3s"
+                        data-bg-color="#f3f6f7">
+                        <!-- <div
+                  class="form-title-box bg-title"
+                  data-bg-src="assets/img/bg/bg-shape-9.jpg"
+                >
+                  <div class="row justify-content-between align-items-center">
+                    <div class="col-xl-10">
+                      <h2 class="h4 mb-2 text-white">Book An Appointment</h2>
+                      <p class="mb-0 text-white-light">
+                        Scenarios and economically sound ROI. Dynamically seize
+                        cross platform "outside the box" thinking after
+                        enterprise...
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div class="form-box">
+                  <div class="row">
+                    <div class="col-lg-6 form-group">
+                      <select class="form-select style3">
+                        <option hidden disabled selected>
+                          Select Services
+                        </option>
+                        <option>Aerospace Medicine</option>
+                        <option>Bariatric Surgery</option>
+                        <option>Infectious Diseases</option>
+                        <option>Laboratory Medicine</option>
+                      </select>
+                    </div>
+                    <div class="col-lg-6 form-group">
+                      <select class="form-select style3">
+                        <option hidden disabled selected>Choose Doctor</option>
+                        <option>Aerospace Medicine</option>
+                        <option>Bariatric Surgery</option>
+                        <option>Infectious Diseases</option>
+                        <option>Laboratory Medicine</option>
+                      </select>
+                    </div>
+                    <div class="col-lg-6 form-group">
+                      <input
+                        type="text"
+                        class="form-control style3"
+                        placeholder="Your Name"
+                      />
+                      <i class="fal small fa-user"></i>
+                    </div>
+                    <div class="col-lg-6 form-group">
+                      <input
+                        type="email"
+                        class="form-control style3"
+                        placeholder="Email Address"
+                      />
+                      <i class="fal small fa-envelope"></i>
+                    </div>
+                    <div class="col-lg-6 form-group">
+                      <input
+                        type="number"
+                        class="form-control style3"
+                        placeholder="Your Phone"
+                      />
+                      <i class="fal small fa-phone"></i>
+                    </div>
+                    <div class="col-lg-6 form-group">
+                      <input
+                        type="text"
+                        class="dateTime-pick form-control style3"
+                        placeholder="Select Date & Time"
+                      />
+                      <i class="fal small fa-calendar-alt"></i>
+                    </div>
+                    <div class="col-xl-12 text-center">
+                      <button type="submit" class="vs-btn style2">
+                        Make Appointment<i class="far fa-calendar-alt"></i>
+                      </button>
+                    </div>
+                  </div> -->
+                </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    </div>
+</section>
+<?php include 'footerhospital.php'; ?>
