dd5929b029d2d1c02bf3f0ae7c3cbd63917e7cc3
Nayeem Hasan Shanto <snayeemhasan@gmail.com>
2025-08-25 18:14:45 +0600

    dr. fahmila's profile updated

--- dr-fahmila.php
+++ dr-fahmila.php
@@ -1,222 +1,119 @@
-<?php include 'headerhospital.php';?>
-    <!--==============================
+<?php include 'headerhospital.php'; ?>
+<!--==============================
     Breadcumb
 ============================== -->
-    <div class="breadcumb-wrapper">
-      <div
-        class="parallax"
-        data-parallax-image="assets/img/breadcurmb/breadcurmb-1-1.jpg"
-      ></div>
-      <div class="container z-index-common">
-        <div class="breadcumb-content">
-          <h1 class="breadcumb-title" style="color: whitesmoke;"></h1>
-          <div class="breadcumb-menu-wrap">
-            <i class="far fa-home-lg"></i>
-            <ul class="breadcumb-menu">
-              <li><a href="index-4.php">Home</a></li>
-              <li><a href="alldoctor.php">Doctor Details</a></li>
-              
-            </ul>
-          </div>
-        </div>
+<div class="breadcumb-wrapper">
+  <div
+    class="parallax"
+    data-parallax-image="assets/img/breadcurmb/breadcurmb-1-1.jpg"></div>
+  <div class="container z-index-common">
+    <div class="breadcumb-content">
+      <h1 class="breadcumb-title">Department of Paediatrics</h1>
+      <div class="breadcumb-menu-wrap">
+        <i class="far fa-home-lg"></i>
+        <ul class="breadcumb-menu">
+          <li><a href="index-4.php">Home</a></li>
+          <li><a href="alldoctor.php">Doctor Details</a></li>
+
+        </ul>
       </div>
     </div>
-    <!--==============================
+  </div>
+</div>
+<!--==============================
     Team Details
     ==============================-->
-    <section class="vs-details-wrapper space-top space-md-bottom">
-      <div class="container">
-        <div class="row gx-40">
-          <div class="col-lg-5">
-            <div
-              class="member-header mb-40 overflow-hidden rounded-3 position-relative"
-            >
-              <div class="member-details-img">
-                <!-- <img
-                  src="assets/img/team/avatar-details-profile.png"
-                  alt="Member Image"
-                  class="w-100"
-                /> -->
-
-                <img
-                  src="assets/img/team/female-details.jpg"
-                  alt="Member Image"
-                  class="w-100"
-                />
-                
-              </div>
-
-              <div class="member-angle-links">
-                <div class="shape">
-                  <img src="assets/img/bg/member-shape.png" alt="Shape" />
-                </div>
-
-              </div>
-
-              <a href="https://verify.bmdc.org.bd/" class="certifate-link"
-                ><span style="color: #07ccec; :hover {color: #FFFFFF;}">BMDC Registration Number: A-30106 </span><i
-                  class="far fa-chevron-circle-right"
-                ></i
-              ></a>
-            </div>
-
-            <div
-              class="team-schedule bg-smoke wow fadeInUp"
-              data-wow-delay="0.3s"
-            >
-              <h3 class="h4 border-title">Time Schedule</h3>
-              <table class="team-schedule-table">
-                <tbody>
-                  <tr>
-                    <td>Saturday - Thursday</td>
-                    <td>10.00 AM - 1.00 PM</td>
-                  </tr>
-                  <tr>
-                    <td>Room No:</td>
-                    <td>N/A</td>
-                  </tr>
-                  
-                  
-                
-                </tbody>
-              </table>
-              <a href="tel:66925682596" class="vs-btn style2"
-                >+88 02 8878080<i class="fas fa-phone"></i
-              ></a>
-            </div>
+<section class="vs-details-wrapper space-top space-md-bottom">
+  <div class="container">
+    <div class="row gx-40">
+      <div class="col-lg-5">
+        <div
+          class="member-header mb-40 overflow-hidden rounded-3 position-relative">
+          <div class="member-details-img">
+            <img
+              src="assets/img/team/female-details.jpg"
+              alt="Member Image"
+              class="w-100" />
           </div>
-          <div class="col-lg-7">
-            <div class="team-content">
-              <h2 class="mb-0 mt-n2">Dr. Fahmila Anjum</h2>
-              <h3 class="text-theme fs-xs">Assistant Professor (CC)<br> Department of Paediatrics</h3>
-              <p class="fs-md text-title" style="text-align: justify;">
-                Dr. Fahmila Anjum is a caring and knowledgeable child specialist with over a decade of clinical experience in paediatrics. As an Assistant Professor (Contractual Category) in the Department of Paediatrics at MH Samorita Hospital & Medical College, she is deeply involved in providing compassionate child healthcare as well as teaching and mentoring future medical professionals. She is especially noted for her work in neonatal care, paediatric respiratory issues and vaccination awareness.
-              </p>
-        
-              
-              <table class="member-table" style="background-color: #f3f6f7;">
-                <tbody>
-                  <tr>
-                    <th><strong>Speciality</strong></th>
-                    <td>Paediatrics</td>
-                  </tr>
-                  <tr>
-                    <th><strong>Degrees</strong></th>
-                    <td>
-                      <ul class="member-info-list">
-                    
-                        <li>MBBS.</li>
-                        <li>DCH.</li>
-                        <li>FCPS (Paediatrics).</li>
-                        <li>MCPS</li>
-                  </tr>
-                  <tr>
-                    <th><strong>Areas of Expertise</strong></th>
-                    <td>
-                      <ul class="member-info-list">
-                            <li>Common childhood infections and fever</li>
-                            <li>Neonatal jaundice and newborn care</li>
-                            <li>Childhood asthma and allergy</li>
-                            <li>Nutritional deficiencies and feeding issues</li>
-                            <li>Diarrhea and dehydration in children</li>
-                            <li>Developmental screening and follow-up</li>
-                            <li>Immunization and vaccine guidance</li>
-                            <li>General paediatric check-ups and counselling</li>
-                      </ul>
-                    </td>
-                  </tr>
-                  <tr>
-                    <th><strong>Academic Qualification</strong></th>
-                    <td>MBBS from Mymensingh Medical College, DCH (Diploma in Child Health) from BMU, and FCPS (Paediatrics) from the Bangladesh College of Physicians and Surgeons (BCPS).</td>
-                  </tr>
-                </tbody>
-              </table>
-              <form
-                action="#"
-                class="form-wrap1 shadow-none mb-30 rounded-3 overflow-hidden wow fadeInUp"
-                data-wow-delay="0.3s"
-                data-bg-color="#f3f6f7"
-              >
-                <!-- <div
-                  class="form-title-box bg-title"
-                  data-bg-src="assets/img/bg/bg-shape-9.jpg"
-                >
-                  <div class="row justify-content-between align-items-center">
-                    <div class="col-xl-10">
-                      <h2 class="h4 mb-2 text-white">Book An Appointment</h2>
-                      <p class="mb-0 text-white-light">
-                        Scenarios and economically sound ROI. Dynamically seize
-                        cross platform "outside the box" thinking after
-                        enterprise...
-                      </p>
-                    </div>
-                  </div>
-                </div>
-                <div class="form-box">
-                  <div class="row">
-                    <div class="col-lg-6 form-group">
-                      <select class="form-select style3">
-                        <option hidden disabled selected>
-                          Select Services
-                        </option>
-                        <option>Aerospace Medicine</option>
-                        <option>Bariatric Surgery</option>
-                        <option>Infectious Diseases</option>
-                        <option>Laboratory Medicine</option>
-                      </select>
-                    </div>
-                    <div class="col-lg-6 form-group">
-                      <select class="form-select style3">
-                        <option hidden disabled selected>Choose Doctor</option>
-                        <option>Aerospace Medicine</option>
-                        <option>Bariatric Surgery</option>
-                        <option>Infectious Diseases</option>
-                        <option>Laboratory Medicine</option>
-                      </select>
-                    </div>
-                    <div class="col-lg-6 form-group">
-                      <input
-                        type="text"
-                        class="form-control style3"
-                        placeholder="Your Name"
-                      />
-                      <i class="fal small fa-user"></i>
-                    </div>
-                    <div class="col-lg-6 form-group">
-                      <input
-                        type="email"
-                        class="form-control style3"
-                        placeholder="Email Address"
-                      />
-                      <i class="fal small fa-envelope"></i>
-                    </div>
-                    <div class="col-lg-6 form-group">
-                      <input
-                        type="number"
-                        class="form-control style3"
-                        placeholder="Your Phone"
-                      />
-                      <i class="fal small fa-phone"></i>
-                    </div>
-                    <div class="col-lg-6 form-group">
-                      <input
-                        type="text"
-                        class="dateTime-pick form-control style3"
-                        placeholder="Select Date & Time"
-                      />
-                      <i class="fal small fa-calendar-alt"></i>
-                    </div>
-                    <div class="col-xl-12 text-center">
-                      <button type="submit" class="vs-btn style2">
-                        Make Appointment<i class="far fa-calendar-alt"></i>
-                      </button>
-                    </div>
-                  </div> -->
-                </div>
-              </form>
+
+          <div class="member-angle-links">
+            <div class="shape">
+              <img src="assets/img/bg/member-shape.png" alt="Shape" />
             </div>
+
           </div>
+
+          <a href="#" class="certifate-link"><span style="color: #07ccec; :hover {color: #FFFFFF;}">BMDC Registration Number: A-30106 </span><i
+              class="far fa-chevron-circle-right"></i></a>
+        </div>
+
+        <div
+          class="team-schedule bg-smoke wow fadeInUp"
+          data-wow-delay="0.3s">
+          <h3 class="h4 border-title">Time Schedule</h3>
+          <table class="team-schedule-table">
+            <tbody>
+              <tr>
+                <td>Saturday - Thursday</td>
+                <td>10.00 AM - 1.00 PM</td>
+              </tr>
+              <tr>
+                <td>Room No:</td>
+                <td>804</td>
+              </tr>
+
+
+
+            </tbody>
+          </table>
+          <a href="tel:66925682596" class="vs-btn style2">+88 02 8878080<i class="fas fa-phone"></i></a>
         </div>
       </div>
-    </section>
-    <?php include 'footerhospital.php';?>
+      <div class="col-lg-7">
+        <div class="team-content">
+          <h2 class="mb-0 mt-n2">Asst. Prof. Dr. Fahmila Anjum</h2>
+          <h3 class="text-theme fs-xs">MBBS, DCH, FCPS (Paediatrics), MCPS <br><br> Assistant Professor(CC)<br> Department of Paediatrics</h3>
+          <p class="fs-md text-title" style="text-align: justify;">
+            <strong>Asst. Prof. Dr. Fahmila Anjum</strong> is a caring and knowledgeable child specialist with over a decade of clinical experience in paediatrics. As an Assistant Professor (Contractual Category) in the Department of Paediatrics at MH Samorita Hospital & Medical College, she is deeply involved in providing compassionate child healthcare as well as teaching and mentoring future medical professionals. She is especially noted for her work in neonatal care, paediatric respiratory issues and vaccination awareness.
+          </p>
+
+
+          <table class="member-table" style="background-color: #f3f6f7;">
+            <tbody>
+              <tr>
+                <th><strong>Specialty</strong></th>
+                <td>Paediatrics</td>
+              </tr>
+              <tr>
+                <th><strong>Degrees</strong></th>
+                <td>
+                  <ul class="member-info-list">
+                    <li>MBBS – Jahurul Islam Medical College, University of Dhaka</li>
+                    <li>DCH – Dhaka Medical College</li>
+                    <li>FCPS (Paediatrics) – Bangladesh College of Physicians & Surgeons (BCPS)</li>
+                    <li>MCPS (Paediatrics) – Bangladesh College of Physicians & Surgeons (BCPS)</li>
+              </tr>
+              <tr>
+                <th><strong>Areas of Expertise</strong></th>
+                <td>
+                  <ul class="member-info-list">
+                    <li>Common childhood infections and fever</li>
+                    <li>Neonatal jaundice and newborn care</li>
+                    <li>Childhood asthma and allergy</li>
+                    <li>Nutritional deficiencies and feeding issues</li>
+                    <li>Diarrhea and dehydration in children</li>
+                    <li>Developmental screening and follow-up</li>
+                    <li>Immunization and vaccine guidance</li>
+                    <li>General paediatric check-ups and counselling</li>
+
+                  </ul>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+      </div>
+    </div>
+  </div>
+  </div>
+</section>
+<?php include 'footerhospital.php'; ?>
