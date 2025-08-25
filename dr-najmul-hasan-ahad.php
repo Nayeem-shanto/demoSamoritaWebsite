60c38b9805f8981b11b77c4813d9378e298ace8e
Nayeem Hasan Shanto <snayeemhasan@gmail.com>
2025-08-25 17:36:36 +0600

    dr. najmul hasan ahad's profile added

---
+++ dr-najmul-hasan-ahad.php
@@ -0,0 +1,119 @@
+<?php include 'headerhospital.php'; ?>
+<!--==============================
+    Breadcumb
+============================== -->
+<div class="breadcumb-wrapper">
+    <div
+        class="parallax"
+        data-parallax-image="assets/img/breadcurmb/breadcurmb-1-1.jpg"></div>
+    <div class="container z-index-common">
+        <div class="breadcumb-content">
+            <h1 class="breadcumb-title">Department of Surgery</h1>
+            <div class="breadcumb-menu-wrap">
+                <i class="far fa-home-lg"></i>
+                <ul class="breadcumb-menu">
+                    <li><a href="index.php">Home</a></li>
+                    <li class="active">Doctors Details</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+<!--==============================
+    Team Details
+    ==============================-->
+<section class="vs-details-wrapper space-top space-md-bottom">
+    <div class="container">
+        <div class="row gx-40">
+            <div class="col-lg-5">
+                <div
+                    class="member-header mb-40 overflow-hidden rounded-3 position-relative">
+                    <div class="member-details-img">
+                        <img
+                            src="./assets/img/team/avatar-details-profile.png"
+                            alt="Member Image"
+                            class="w-100" />
+                    </div>
+
+                    <div class="member-angle-links">
+                        <div class="shape">
+                            <img src="assets/img/bg/member-shape.png" alt="Shape" />
+                        </div>
+                        <span class="middle-icon">
+                            <!-- <i class="fas fa-share-alt"></i> -->
+                        </span>
+                        <!-- <a href="#"><i class="fab fa-facebook-f"></i></a>
+                <a href="#"><i class="fab fa-twitter"></i></a>
+                <a href="#"><i class="fab fa-google"></i></a>
+                <a href="#"><i class="fab fa-linkedin-in"></i></a> -->
+                    </div>
+
+                    <a href="https://verify.bmdc.org.bd/" class="certifate-link"><span style="color: #07ccec; :hover {color: #FFFFFF;}">BMDC Registration Number: A-120254 </span>
+                        <i class="far fa-chevron-circle-right"></i></a>
+                </div>
+
+                <div
+                    class="team-schedule bg-smoke wow fadeInUp"
+                    data-wow-delay="0.3s">
+                    <h3 class="h4 border-title">Time Schedule</h3>
+                    <table class="team-schedule-table">
+                        <tbody>
+                            <tr>
+                                <td>Saturday - Thursday:</td>
+                                <td>10.00 am - 12.30 pm</td>
+                            </tr>
+                            <tr>
+                                <td>Room No:</td>
+                                <td>209</td>
+                            </tr>
+
+                        </tbody>
+                    </table>
+                    <a href="tel:66925682596" class="vs-btn style2">+88 02 8878080<i class="fas fa-phone"></i></a>
+                </div>
+            </div>
+            <div class="col-lg-7">
+                <div class="team-content">
+                    <h2 class="mb-0 mt-n2">Dr. Md. Najmul Hasan Ahad</h2>
+                    <p class="text-theme fs-xs">MBBS<br><br>Assistant Registrar<br>Department of Surgery</p>
+                    <div style="text-align: justify;">
+                        <p class="fs-md text-title">
+                            <strong>Dr. Md. Najmul Hasan Ahad</strong> is a committed and detail-oriented general surgeon currently serving as Assistant Registrar in the Department of Surgery at MH Samorita Hospital & Medical College. He plays an important role in clinical evaluation, surgical assistance and post-operative management. Dr. Ahad is known for his disciplined approach, reliability and patient-focused care. He is actively involved in both routine and emergency surgical services under the guidance of senior consultants.
+                        </p>
+                    </div>
+                    <table class="member-table">
+                        <tbody style="background-color: #f3f6f7;">
+                            <tr>
+                                <th><strong>Specialty</strong></th>
+                                <td> General Surgery</td>
+                            </tr>
+                            <tr>
+                                <th><strong>Degrees</strong></th>
+                                <td>
+                                    <ul class="member-info-list">
+                                        <li>MBBS - DU</li>
+                                    </ul>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th><strong>Areas of Expertise</strong></th>
+                                <td>
+                                    <ul class="member-info-list">
+                                        <li>Pre and post-operative surgical care</li>
+                                        <li>Minor surgical procedures</li>
+                                        <li>Wound and abscess management</li>
+                                        <li>Assistance in laparoscopic and general surgery</li>
+                                        <li>OPD and inpatient evaluation</li>
+                                        <li>Trauma and emergency care</li>
+                                        <li>Follow-up and recovery support</li>
+                                    </ul>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>                  
+            </div>
+        </div>
+    </div>
+    </div>
+</section>
+<?php include 'footerhospital.php'; ?>
